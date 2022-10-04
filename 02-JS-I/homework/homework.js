4901cc1554d6ddf5e6f8a84a9c08a7f9aef3639c
cobandoorti <mauricioobando87@hotmail.com>
2022-10-03 23:48:38 -0500

    se guradaron cambios text

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -226,46 +226,62 @@ if (numero > 0 ){  return "Es positivo";}
  
 if ( numero < 0 ) { return "Es negativo";}
 
-if ( numero == 0 ) { return false; }
+if ( numero == 0 ) { return false; }  
 
 }
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+
+  return str + "!";
+
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+
+  return nombre +" "+ apellido;
+
 }
+
+
+
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  return "Hola "+nombre+"!"; 
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
   
+  return area_rectangulo = alto * ancho;
+
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+ 
+  return perimetro = 4 * lado;
   
+ 
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+
+ return area_triangulo = base * altura / 2;
+
 
 }
 
@@ -275,6 +291,10 @@ function deEuroAdolar(euro){
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
   
+return  cambio_dolares = euro * +" "+ / 1;
+
+
+
 }
 
 
